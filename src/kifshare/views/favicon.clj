720be6270a270c0668b32ac445e859bcd18c0ab0
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-01-07 13:06:44 -0700

    Removed unneeded files.

--- src/kifshare/views/favicon.clj
+++
@@ -1,7 +0,0 @@
-(ns kifshare.views.favicon
-  (:use [noir.core :only [defpage pre-route]] 
-        [noir.response :only [status redirect]]))
-
-(pre-route "/favicon.ico"
-  []
-  (status 404 "I ain't got no favicon.ico, hyuk."))
